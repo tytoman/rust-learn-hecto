cd5f6e61a415e883ef7b7817bc5ffa24a519561f
TytoMan <47035388+TytoMan@users.noreply.github.com>
2024-12-20 18:58:23 +0900

    Handling Error

--- src/main.rs
+++ src/main.rs
@@ -6,18 +6,25 @@ fn main() {
     enable_raw_mode().unwrap();
 
     for b in io::stdin().bytes() {
-        let b = b.unwrap();
-        let c = b as char;
+        match b {
+            Ok(b) => {
+                let c = b as char;
+        
+                if c.is_control() {
+                    println!("Binary: {0:08b} ASCII: {0:#03} \r", b);
+                } else {
+                    println!("Binary: {0:08b} ASCII: {0:#03} Character: {1:#?} \r", b, c);
+                }
 
-        if c.is_control() {
-            println!("Binary: {0:08b} ASCII: {0:#03} \r", b);
-        } else {
-            println!("Binary: {0:08b} ASCII: {0:#03} Character: {1:#?} \r", b, c);
-        }
-
-        if c == 'q' {
-            disable_raw_mode().unwrap();
-            break;
+                if c == 'q' {
+                    break;
+                }
+            }
+            Err(e) => {
+                println!("Error: {0:#?} \r", e);
+            }
         }
     }
+
+    disable_raw_mode().unwrap();
 }
